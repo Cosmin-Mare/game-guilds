2ac2c48bfbfac764cf49caf67f73ecb27c0a1d8d
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2025-01-07 15:10:42 +0200

    Make steps section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Heading from "@/components/heading";
+import Steps from "@/components/steps";
 
 export default function Home() {
   return (
-    <div>
+    <div>   
       <Heading/>
+      <Steps />
     </div>
   );
 }
