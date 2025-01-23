5ead8e857c0c543b01b527c387acf2a8729dd7a8
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2025-01-23 02:54:35 +0200

    add confetti and animation

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,44 @@
 "use client";
 
-import { useState } from "react";
-import styles from "./heading.module.css";
+import { useRef, useState } from "react";
+import styles from "./header.module.css";
+import Explosion from "react-canvas-confetti/dist/presets/explosion";
+import { TConductorInstance, TDecorateOptionsFn } from "react-canvas-confetti/dist/types";
 
 export default function Header() {
     const [email, setEmail] = useState("");
     const [feedbackMessage, setFeedbackMessage] = useState("");
+    const feedbackMessageRef = useRef<HTMLInputElement>(null);
     const [isSubmitting, setIsSubmitting] = useState(false);
+    const controller = useRef<TConductorInstance>(null);
+
+    const onInitHandler = ({ conductor }: { conductor: TConductorInstance }) => {
+        controller.current = conductor;
+      };
+    
+    const onShoot = () => {
+        if(!controller.current) return;
+        console.log(controller.current)
+        controller.current.shoot();
+    };
+
+    function animateFeedbackMessage() {
+        if(!feedbackMessageRef.current) return
+        feedbackMessageRef.current.style.transform = "translate(0, 0)";
+    }
+
+    const decorateOptions : TDecorateOptionsFn = (defaultOptions) => {
+        return {
+            particleCount: 100,
+            angle: 90,
+            spread: 180,
+            decay: .92,
+            gravity: 2.2,
+            drift: 0,
+            ticks: 600,
+            origin: {x: .5, y: .7}
+        }
+    }
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         if (isSubmitting) {
@@ -14,7 +46,7 @@ export default function Header() {
         }
         setIsSubmitting(true);
         event.preventDefault();
-
+        onShoot();
         fetch("/api/rsvp", {
             method: "POST",
             headers: {
@@ -25,16 +57,19 @@ export default function Header() {
             .then((response) => {
                 if (response.ok) {
                     setFeedbackMessage("Thanks for RSVPing!");
+                    animateFeedbackMessage()
                     setEmail("");
                     setIsSubmitting(false);
                 } else {
                     setFeedbackMessage("Something went wrong. Please try again.");
+                    animateFeedbackMessage()
                     setIsSubmitting(false);
                 }
             })
             .catch((error) => {
                 console.error("Error:", error);
                 setFeedbackMessage("Something went wrong. Please try again.");
+                animateFeedbackMessage()
                 setIsSubmitting(false);
             });
     };
@@ -49,18 +84,23 @@ export default function Header() {
             <h2>Receive grants to improve and publish them</h2>
             <section id="rsvp" className={styles.rsvp}>
                 <h2>RSVP here for future updates!</h2>
-                <form id="notifyForm" onSubmit={handleSubmit}>
-                    <input 
-                        type="email"
-                        name="email"
-                        placeholder="orpheus@hackclub.com" 
-                        required 
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <button type="submit">RSVP</button>
-                </form>
-                <p id="feedbackMessage">{feedbackMessage}</p>
+                <div className={styles.formContainer}>
+                    <form id="notifyForm" onSubmit={handleSubmit}>
+                        <input 
+                            type="email"
+                            name="email"
+                            placeholder="orpheus@hackclub.com" 
+                            required 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <Explosion width={100} height={100} onInit={onInitHandler} decorateOptions={decorateOptions}/>
+                        <button type="submit">RSVP</button>
+                    </form>
+                    <div className={styles.feedbackMessageContainer}>
+                        <p id="feedbackMessage" className={styles.feedbackMessage} ref={feedbackMessageRef}>{feedbackMessage}</p>
+                    </div>
+                </div>
             </section>
             <div className={styles.buttonContainer}>
                 <button>Get your grant!</button>
